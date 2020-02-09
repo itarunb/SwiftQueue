18fa259f3201ff665994a795d0f6e5832420d5e3
itarunb <tarun.bhargava2890@gmail.com>
2020-02-09 14:13:28 +0530

    Update Swift Queue.swift

--- Swift Queue.swift
+++ Swift Queue.swift
@@ -9,17 +9,19 @@
 import Foundation
 
 struct Queue <T> {
-    private var array : [T] = [T]()
-    private var startIndex  : Int = -1
-    private var endIndex    : Int = -1
-    private var size        : Int = 0
+   private var array : [T] = [T]()
+   private var startIndex  : Int?
+   private var endIndex    : Int?
+   //This keeps track of the number of Queue elements(Note that array size can be different than the queue size as we dont
+   // remove the elements from array on dequeue but just move our start and end pointers)
+   private var size        : Int = 0
     
-    var isEmpty : Bool {
+   var isEmpty : Bool {
         return size == 0
     }
     
-    public func peek() -> T? {
-        guard !isEmpty else {
+   public func peek() -> T? {
+        guard !isEmpty,let startIndex = startIndex else {
             return nil
         }
     
@@ -29,27 +31,36 @@ struct Queue <T> {
    public mutating func enqueue(_ element: T) {
         array.append(element)
         size += 1
-        endIndex = endIndex + 1
-        startIndex = endIndex + 1 - size //This takes care of we not having to worry about initial startindex
+       //If an endpoint is there just move it further otherwise point to last element of array we just appended an element to
+        if let index = endIndex {
+            endIndex   = index + 1
+        } else {
+            endIndex   = array.count - 1
+        }
+       //We need to modify startIndex too here as it will be nil when enqueue executes first time 
+        startIndex = array.count - size
     }
     
-   public mutating func dequeue() -> T? {
-        guard size > 0 else {
+  public mutating func dequeue() -> T? {
+        guard !isEmpty,let start = startIndex,let end = endIndex else {
             return nil
         }
         
-        let element = array[startIndex]
+        let element = array[start]
         size -= 1
-        startIndex = size == 0 ? endIndex : startIndex + 1 //We have to ensure startindex doesnt get beyond endIndex when last element in queue has been dequeued
+      //Move the startIndex further in array if queue is non-empty else set it to nil
+        startIndex = isEmpty ? nil : start + 1
+       //Set endIndex to nil if queue is empty
+        endIndex   = isEmpty ? nil : endIndex
         return element
     }
     
    public func printQueue() {
-        guard !isEmpty else {
-            print("Queue is empty")
+        guard !isEmpty,let start = startIndex,let end = endIndex else {
+            print("Empty Queue")
             return
         }
-        for i in startIndex...endIndex {
+        for i in start...end {
             print(array[i])
         }
     }
